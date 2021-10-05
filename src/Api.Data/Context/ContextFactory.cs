6986fef2f3bc5ba3cee34dbee393c790b3cacf4c
Vitor.Ochoa <vitor.ochoa@sequor.com.br>
2021-10-05 20:56:26 -0300

    Criada Arquitetura e implementação camada Api.Data

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrações
+            var connectionString = "Persist Security Info=True;Initial Catalog=dbAPI;Data Source=SQO-036\\SQLEXPRESS;User Id=sa;Password=Chocolatra0$;";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
