ca29d500298ce5d96e6b5f3358accd22320f7060
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-28 19:53:01 -0300

    WIP

---
+++ src/Api.Application.Test/User/UpdateTest/ReturnBadRequest.cs
@@ -0,0 +1,52 @@
+﻿using Api.Application.Controllers;
+using Api.Domain.Dtos;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Application.Test.User.UpdateTest
+{
+    public class ReturnBadRequest
+    {
+        private UsersController _controller;
+
+        [Fact]
+        public async Task CallUpdate()
+        {
+            var serviceMock = new Mock<IUserService>();
+
+            var name = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Put(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new UserDtoUpdateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow,
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Email", "Mensagem de Erro");
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Email = email
+            };
+
+            var result = await _controller.Put(userDtoUpdate);
+
+            Assert.True(result is BadRequestObjectResult);
+        }
+    }
+}
