0e3def3e6c5ea9b08fd737ad677e4ee63b607e95
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-29 02:06:50 -0300

    WIP

--- src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -2,6 +2,10 @@ using Api.Domain.Dtos;
 using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
 using AutoMapper;
+using Domain.Dtos.Cep;
+using Domain.Dtos.Municipio;
+using Domain.Dtos.Uf;
+using Domain.Entities;
 
 namespace Api.CrossCutting.Mappings
 {
@@ -14,6 +18,22 @@ namespace Api.CrossCutting.Mappings
             CreateMap<UserDtoCreateResult, UserEntity>().ReverseMap();
 
             CreateMap<UserDtoUpdateResult, UserEntity>().ReverseMap();
+
+            CreateMap<UfDto, UfEntity>().ReverseMap();
+
+            CreateMap<MunicipioDto, MunicipioEntity>().ReverseMap();
+
+            CreateMap<MunicipioDtoCompleto, MunicipioEntity>().ReverseMap();
+
+            CreateMap<MunicipioDtoCreateResult, MunicipioEntity>().ReverseMap();
+
+            CreateMap<MunicipioDtoUpdateResult, MunicipioEntity>().ReverseMap();
+
+            CreateMap<CepDto, CepEntity>().ReverseMap();
+
+            CreateMap<CepDtoCreateResult, CepEntity>().ReverseMap();
+
+            CreateMap<CepDtoUpdateResult, CepEntity>().ReverseMap();
         }
     }
 }
