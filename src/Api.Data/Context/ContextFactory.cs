49a6030ac6d4fe82ef493158df13b4cceb99d8f8
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-29 01:24:39 -0300

    WIP

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,7 +8,7 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Usado para Criar as Migrações
-            var connectionString = "Persist Security Info=True;Initial Catalog=dbAPI;Data Source=SQO-036\\SQLEXPRESS;User Id=sa;Password=Chocolatra0$;";
+            var connectionString = "Persist Security Info=True;Initial Catalog=dbAPI;Data Source=LAPTOP-F8SESO86\\SQLEXPRESS;Integrated Security=SSPI;";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseSqlServer(connectionString);
             return new MyContext(optionsBuilder.Options);
