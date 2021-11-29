e94483514dba5f03464ccbf1743945f08c01a979
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-28 21:52:25 -0300

    WIP

---
+++ src/Api.Domain/Entities/UfEntity.cs
@@ -0,0 +1,23 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UfEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(2)]
+        public string Sigla { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string Name { get; set; }
+
+        public IEnumerable<MunicipioEntity> Municipios { get; set; }
+    }
+}
