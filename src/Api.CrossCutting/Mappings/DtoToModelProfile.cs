27667ea85e1d2d7ecdae3941ff925c42cb52e4ee
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-29 02:25:58 -0300

    WIP

--- src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -2,6 +2,10 @@ using Api.Domain.Dtos;
 using Api.Domain.Dtos.User;
 using Api.Domain.Models;
 using AutoMapper;
+using Domain.Dtos.Cep;
+using Domain.Dtos.Municipio;
+using Domain.Dtos.Uf;
+using Domain.Models;
 
 namespace Api.CrossCutting.Mappings
 {
@@ -17,6 +21,22 @@ namespace Api.CrossCutting.Mappings
 
             CreateMap<UserModel, UserDtoUpdate>()
                 .ReverseMap();
+
+            CreateMap<UfDto, UfModel>().ReverseMap();
+
+            CreateMap<MunicipioDto, MunicipioModel>().ReverseMap();
+
+            CreateMap<MunicipioDtoCompleto, MunicipioModel>().ReverseMap();
+
+            CreateMap<MunicipioDtoCreate, MunicipioModel>().ReverseMap();
+
+            CreateMap<MunicipioDtoUpdate, MunicipioModel>().ReverseMap();
+
+            CreateMap<CepDto, CepModel>().ReverseMap();
+
+            CreateMap<CepDtoCreate, CepModel>().ReverseMap();
+
+            CreateMap<CepDtoUpdate, CepModel>().ReverseMap();
         }
     }
 }
