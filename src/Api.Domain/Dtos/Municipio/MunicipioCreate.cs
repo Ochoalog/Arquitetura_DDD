be4ecbefa86e6525eb0909e53820eda898677fb3
vitor.ochoa <vitor.ochoa@freehub.com.br>
2021-11-28 23:14:19 -0300

    WIP

---
+++ src/Api.Domain/Dtos/Municipio/MunicipioCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos.Municipio
+{
+    public class MunicipioCreate
+    {
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; }
+        [Range(0, int.MaxValue)]
+        public int CodIBGE { get; set; }
+        [Required]
+        public Guid UfId { get; set; }
+    }
+}
---
+++ src/Api.Domain/Dtos/Municipio/MunicipioDtoUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos.Municipio
+{
+    public class MunicipioDtoUpdate
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; }
+        [Range(0, int.MaxValue)]
+        public int CodIBGE { get; set; }
+        [Required]
+        public Guid UfId { get; set; }
+    }
+}
